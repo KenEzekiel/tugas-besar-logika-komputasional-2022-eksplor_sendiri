aaa8fb27dc431547390e8d06bcbc6bbe83fffbfd
arsaizdihar <arsadihar@gmail.com>
2022-11-09 17:38:56 +0700

    Change some write to format

--- src/map_rules.pl
+++ src/map_rules.pl
@@ -26,12 +26,12 @@ doNothing.
 showPropertyStatus(Location) :- 
   curPropertyState(Location, Owner, Property) ->  
   (
-    write('Kepemilikan\t\t: Player '), write(Owner), 
+    format('Kepemilikan\t\t: Player ~w', [Owner]), nl, 
     propertyRent(Location, Property, RentCost),
-    write('\nBiaya Sewa Saat Ini\t: '), write(RentCost), 
+    format('Biaya Sewa Saat Ini\t: ~d', [RentCost]), nl, 
     acquisitionPrice(Location, Property, AcCost),
-    (AcCost \= -1 -> write('\nBiaya Akuisisi\t\t: '), write(AcCost) ; doNothing), 
-    write('\nTingkatan Properti\t: '),
+    (AcCost \= -1 -> format('Biaya Akuisisi\t\t: ~d', [AcCost]) ; doNothing), nl,
+    write('Tingkatan Properti\t: '),
     Property = 0 -> write('Tanah') ; 
     (Property = 4 -> write('Landmark') ; 
     write('Bangunan '), write(Property))
@@ -45,15 +45,17 @@ checkLocationDetail(Location) :-
 checkPropertyDetail(Location) :- 
   showLocNameNDesc(Location),
   propertyPrice(Location, [PTanah, PBangunan1, PBangunan2, PBangunan3, PLandmark]),
-write('Harga Tanah\t\t: '), write(PTanah),  write('\nHarga Bangunan 1\t: '), write(PBangunan1),
-  write('\nHarga Bangunan 2\t: '), write(PBangunan2),
-  write('\nHarga Bangunan 3\t: '), write(PBangunan3),
-  write('\nHarga Landmark\t\t: '), write(PLandmark),
-propertyRent(Location, [RTanah, RBangunan1, RBangunan2, RBangunan3, RLandmark]),  write('\n\nBiaya Sewa Tanah\t: '), write(RTanah),
-  write('\nBiaya Sewa Bangunan 1\t: '), write(RBangunan1),
-  write('\nBiaya Sewa Bangunan 2\t: '), write(RBangunan2),
-  write('\nBiaya Sewa Bangunan 3\t: '), write(RBangunan3),
-  write('\nBiaya Sewa Landmark\t: '), write(RLandmark).
+  format('Harga Tanah\t\t: ~d', [PTanah]), nl,
+  format('Harga Bangunan 1\t: ~d', [PBangunan1]), nl,
+  format('Harga Bangunan 2\t: ~d', [PBangunan2]), nl,
+  format('Harga Bangunan 3\t: ~d', [PBangunan3]), nl,
+  format('Harga Landmark\t\t: ~d', [PLandmark]), nl, nl,
+  propertyRent(Location, [RTanah, RBangunan1, RBangunan2, RBangunan3, RLandmark]),  
+  format('Biaya Sewa Tanah\t: ~d', [RTanah]), nl,
+  format('Biaya Sewa Bangunan 1\t: ~d', [RBangunan1]), nl,
+  format('Biaya Sewa Bangunan 2\t: ~d', [RBangunan2]), nl,
+  format('Biaya Sewa Bangunan 3\t: ~d', [RBangunan3]), nl,
+  format('Biaya Sewa Landmark\t: ~d', [RLandmark]).
 
 
 writeLocationStatus(Location) :- 
