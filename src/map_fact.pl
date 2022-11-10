579ee684d459ee9bd245957106270b848d2dd721
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-11-10 14:38:37 +0700

    update location theme

--- src/map_fact.pl
+++ src/map_fact.pl
@@ -3,96 +3,96 @@ board([go, a1, a2, a3, cc1, b1, b2, b3, jl, c1, c2, c3, tx1, d1, d2, d3, fp, e1,
 boardLength(32).
 
 /* a1 */
-showLocNameNDesc(a1) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(a1) :- write('\nNama Lokasi\t\t: Mondstadt\n'),
+  write('Deskripsi Lokasi\t: Pusat wilayah Mondstadt\n\n').
 
 /* a2 */
-showLocNameNDesc(a2) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(a2) :- write('\nNama Lokasi\t\t: Dawn Winery\n'),
+  write('Deskripsi Lokasi\t: Wilayah pembuat wine\n\n').
 
 /* a3 */
-showLocNameNDesc(a3) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(a3) :- write('\nNama Lokasi\t\t: Springvale\n'),
+  write('Deskripsi Lokasi\t: Desa pemburu\n\n').
 
 /* b1 */
-showLocNameNDesc(b1) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(b1) :- write('\nNama Lokasi\t\t: Stormterror s Lair\n'),
+  write('Deskripsi Lokasi\t: Sarang naga\n\n').
 
 /* b2 */
-showLocNameNDesc(b2) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(b2) :- write('\nNama Lokasi\t\t: Wolvendom\n'),
+  write('Deskripsi Lokasi\t: Makam arwah serigala\n\n').
 
 /* b3 */
-showLocNameNDesc(b3) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(b3) :- write('\nNama Lokasi\t\t: Dragonspine\n'),
+  write('Deskripsi Lokasi\t: Wilayah bersalju\n\n').
 
 /* c1 */
-showLocNameNDesc(c1) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(c1) :- write('\nNama Lokasi\t\t: Qingce Village\n'),
+  write('Deskripsi Lokasi\t: Tempat pensiun orang tua\n\n').
 
 /* c2 */
-showLocNameNDesc(c2) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(c2) :- write('\nNama Lokasi\t\t: Wangshu Inn\n'),
+  write('Deskripsi Lokasi\t: Tempat pemberhentian pedagang\n\n').
 
 /* c3 */
-showLocNameNDesc(c3) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(c3) :- write('\nNama Lokasi\t\t: Liyue Harbor\n'),
+  write('Deskripsi Lokasi\t: Pusat wilayah Liyue\n\n').
 
 /* d1 */
-showLocNameNDesc(d1) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(d1) :- write('\nNama Lokasi\t\t: Jade Chamber\n'),
+  write('Deskripsi Lokasi\t: Istana Ningguang\n\n').
 
 /* d2 */
-showLocNameNDesc(d2) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(d2) :- write('\nNama Lokasi\t\t: The Chasm\n'),
+  write('Deskripsi Lokasi\t: Wilayah penambangan\n\n').
 
 /* d3 */
-showLocNameNDesc(d3) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(d3) :- write('\nNama Lokasi\t\t: Qingyun Peak\n'),
+  write('Deskripsi Lokasi\t: Rumahnya para adeptus\n\n').
 
 /* e1 */
-showLocNameNDesc(e1) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(e1) :- write('\nNama Lokasi\t\t: Inazuma City\n'),
+  write('Deskripsi Lokasi\t: Pusat wilayah Inazuma\n\n').
 
 /* e2 */
-showLocNameNDesc(e2) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(e2) :- write('\nNama Lokasi\t\t: Ritou\n'),
+  write('Deskripsi Lokasi\t: Pelabuhan Inazuma\n\n').
 
 /* e3 */
-showLocNameNDesc(e3) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(e3) :- write('\nNama Lokasi\t\t: Kamisato Estate\n'),
+  write('Deskripsi Lokasi\t: Perumahan Kamisato\n\n').
 
 /* f1 */
-showLocNameNDesc(f1) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(f1) :- write('\nNama Lokasi\t\t: Grand Narukami Shrine\n'),
+  write('Deskripsi Lokasi\t: Kuil penjaga pohon sakura keramat\n\n').
 
 /* f2 */
-showLocNameNDesc(f2) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(f2) :- write('\nNama Lokasi\t\t: Kujou Encampment\n'),
+  write('Deskripsi Lokasi\t: Benteng Kujou\n\n').
 
 /* f3 */
-showLocNameNDesc(f3) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(f3) :- write('\nNama Lokasi\t\t: Sangonomiya Shrine\n'),
+  write('Deskripsi Lokasi\t: Markas tentara perlawanan\n\n').
 
 /* g1 */
-showLocNameNDesc(f1) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(g1) :- write('\nNama Lokasi\t\t: Gandharva Ville\n'),
+  write('Deskripsi Lokasi\t: Desa penjaga hutan\n\n').
 
 /* g2 */
-showLocNameNDesc(f2) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(g2) :- write('\nNama Lokasi\t\t: Sumeru City\n'),
+  write('Deskripsi Lokasi\t: Pusat wilayah Sumeru\n\n').
 
 /* g3 */
-showLocNameNDesc(f3) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(g3) :- write('\nNama Lokasi\t\t: Port Osmos\n'),
+  write('Deskripsi Lokasi\t: Pelabuhan pusat Sumeru\n\n').
 
 /* h1 */
-showLocNameNDesc(f1) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(h1) :- write('\nNama Lokasi\t\t: Caravan Ribat\n'),
+  write('Deskripsi Lokasi\t: Pembatas sumeru dengan wilayah padang pasir\n\n').
 
 /* h2 */
-showLocNameNDesc(f2) :- write('\nNama Lokasi\t\t: Jakarta\n'),
-  write('Deskripsi Lokasi\t: Ibukota Indonesia\n\n').
+showLocNameNDesc(h2) :- write('\nNama Lokasi\t\t: Aaru Village\n'),
+  write('Deskripsi Lokasi\t: Wilayah pengikut Raja Scarlet dan tempat pembuangan pelajar Sumeru\n\n').
 
 propertyPrices(a1, Prices) :- Prices = [200, 1000, 2000, 3000, 3000].
 propertyPrices(a2, Prices) :- Prices = [200, 1000, 2000, 3000, 3000].
