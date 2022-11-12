3fe96475dd54cf5aa02e497cffbe88dd8bf82d2a
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-11-12 14:23:29 +0700

    wip: bankruptcy

--- src/rent.pl
+++ src/rent.pl
@@ -1,3 +1,22 @@
+:- dynamic(unresolvedBankruptcy/1).
+:- dynamic(unresolvableBankruptcy/1).
+
+declarePendingBankruptcy(Player) :- 
+    retractall(unresolvedBankruptcy(_)),
+    asserta(unresolvedBankruptcy(Player)).
+
+resolveBankruptcy(Player) :- 
+    retractall(unresolvedBankruptcy(Player)).
+
+declarePermanentBankruptcy(Player) :- 
+    retractall(unresolvableBankruptcy(_)),
+    asserta(unresolvableBankruptcy(Player)).
+
+cashableWorth(Player, Cash) :- % jumlah cash sekarang + cash hasil penjualan semua asset (80%*totalAsset)
+  balance(Player, Balance),
+  totalAsset(Player, Asset),
+  Cash is Balance + 0.8*Asset.
+
 isTileOwnedBy(Tile, Player) :- 
     curPropertyState(Tile, Owner, _),
     Owner =:= Player.
@@ -12,6 +31,37 @@ isAbleToPayRent(Tile, Player) :- % pastikan tile sudah dimiliki oleh player lawa
     Balance >= Rent.
 
 payRent(Tile, Payer) :- % pastikan player bisa bayar rent. Periksa dengan relasi isAbleToPayRent
-    rentAmount(Tile, Owner, Rent),
-    addBalance(Owner, Rent),
-    subtractBalance(Payer, Rent).
+    isAbleToPayRent(Tile, Payer) -> (
+        rentAmount(Tile, Owner, Rent),
+        addBalance(Owner, Rent),
+        subtractBalance(Payer, Rent)
+    ) ; (
+        rentAmount(Tile, _, Rent),
+        cashableWorth(Payer, Worth),
+        (Worth >= Rent) -> (
+            write('Wah, uangmu kurang! Apakah kamu ingin tetap melanjutkan permainan?'), nl,
+            declarePendingBankruptcy(Payer)
+        ) ; (
+            declarePermanentBankruptcy(Payer),
+            write('Sayang sekali, uangmu sudah tidak cukup. Selamat tinggal :)'), nl
+        )
+    ).
+
+% writeAssetList(Tile, No) :- 
+%     curPropertyState(Tile, _, PropertyLevel),
+%     format('~d. ~w bangunan ~d : ', [No, Tile, PropertyLevel]), nl.
+
+% displayAssets(Player, [H|T]) :-
+
+
+% sellAsset(Player, Index) :-
+
+
+tidak :-
+    unresolvedBankruptcy(Player),
+    resolveBankruptcy(Player),
+    declarePermanentBankruptcy(Player), % Permainan selesai. Reset ???
+    write('Salah satu pemain telah menyatakan bangkrut, sehingga permainan selesai'), nl.
+
+lanjut :-
+    unresolvedBankruptcy(Player).
