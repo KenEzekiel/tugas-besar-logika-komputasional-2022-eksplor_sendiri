3b6433cce566ccce2074b53c17ffd78d8e410786
Fatih20 <fnri39@protonmail.com>
2022-11-14 00:48:49 +0700

    Initial work on inventory and selling and buying tiles.

--- src/aset.pl
+++ src/aset.pl
@@ -1,29 +1,31 @@
 :- dynamic(tileAsset/2).
 
-tileAsset(a1,0).
-tileAsset(a2,0).
-tileAsset(a3,0).
-tileAsset(b1,0).
-tileAsset(b2,0).
-tileAsset(b3,0).
-tileAsset(c1,0).
-tileAsset(c2,0).
-tileAsset(c3,0).
-tileAsset(d1,0).
-tileAsset(d2,0).
-tileAsset(d3,0).
-tileAsset(e1,0).
-tileAsset(e2,0).
-tileAsset(e3,0).
-tileAsset(f1,0).
-tileAsset(f2,0).
-tileAsset(f3,0).
-tileAsset(g1,0).
-tileAsset(g2,0).
-tileAsset(g3,0).
-tileAsset(h1,0).
-tileAsset(h2,0).
+inventory(v, []).
+inventory(w, []).
 
+tileAsset(a1,-2).
+tileAsset(a2,-2).
+tileAsset(a3,-2).
+tileAsset(b1,-2).
+tileAsset(b2,-2).
+tileAsset(b3,-2).
+tileAsset(c1,-2).
+tileAsset(c2,-2).
+tileAsset(c3,-2).
+tileAsset(d1,-2).
+tileAsset(d2,-2).
+tileAsset(d3,-2).
+tileAsset(e1,-2).
+tileAsset(e2,-2).
+tileAsset(e3,-2).
+tileAsset(f1,-2).
+tileAsset(f2,-2).
+tileAsset(f3,-2).
+tileAsset(g1,-2).
+tileAsset(g2,-2).
+tileAsset(g3,-2).
+tileAsset(h1,-2).
+tileAsset(h2,-2).
 
 colorGroup(brown, [a1, a2, a3]).
 colorGroup(cyan, [b1, b2, b3]).
@@ -38,6 +40,19 @@ tileAssetUpdater(Tile, NS):-
     retractall(tileAsset(Tile, _)),
     asserta(tileAsset(Tile, NS)).
 
+inventoryUpdater(P, NewInventory):-
+    retractall(inventory(Tile, _)),
+    asserta(inventory(Tile, NewInventory)).
+
+inventoryAppender(P, NewTile):-
+    inventory(P, Inventory),
+    insertElmtLast(Inventory, NewTile, NewInventory),
+    inventoryUpdater(P, NewInventory).
+
+inventoryDeleter(P, NI):-
+    inventory(P, Inventory),
+    deleteElmt(Inventory, NewTile, NewInventory),
+    inventoryUpdater(P, NewInventory).
 
 colorGroupOfTile(Tile, GroupName) :-
     colorGroup(GroupName, TileList),
@@ -47,21 +62,41 @@ tileListOfTile(Tile, TileList) :-
     colorGroup(GroupName, TileList),
     isElmt(Tile, TileList), !.
 
-completeSet(P, Tile):-
+completeSet(P, Tile, Res):-
     colorGroupOfTile(Tile, GroupName),
     colorGroup(GroupName, TileList),
     inventory(P, Inventory),
-    \+ (isElmt(Tile_C, TileList), \+ isElmt(Tile_C, Inventory))
+    (\+ (isElmt(Tile_C, TileList, 1), isElmt(Tile_C, Inventory, 0)) -> Res is 1; Res is 0).
+
+canRedeemBasicCheck(P, Tile, 0).
+
+canRedeemBasicCheck(P, Tile, 1) :-
+    turn(P, 1),
+    inventory(P, Inventory),
+    isElmt(Tile,Inventory, 1), !.
 
 canBuyBasicCheck(P, Tile, Res):-
     (turn(P, 1),
     inventory(P, Inventory),
-    isElmt(Tile,Inventory), completeSet(P, Tile)) -> Res is 1 ; Res is 0.
+    isElmt(Tile,Inventory, 1), completeSet(P, Tile, 1)) -> Res is 1 ; Res is 0, !.
 
 equalityCheck(Tile, Equality) :-
     tileAsset(Tile, TileAsset),
     tileListOfTile(Tile, TileList),
-    \+ (isElmt(X, TileList), tileAsset(X, XAsset), X < TileAsset).
+    \+ (isElmt(X, TileList, 1), tileAsset(X, XAsset), X < TileAsset).
+
+buyTile(P, Tile):-
+    inventory(P, Inventory),
+    \+ (isElmt(Tile, Inventory, 1)),
+    inventoryAppender(P, Tile).
+    tileAssetUpdater(Tile, 0).
+
+buyAset(P, Tile, m):-
+    canRedeemBasicCheck(P, Tile, 1),
+    tileAsset(Tile, TileAsset),
+    TileAsset =:= -1,
+    NTA is TileAsset + 1,
+    tileAssetUpdater(Tile, NTA).
 
 buyAset(P, Tile, r):-
     canBuyBasicCheck(P, Tile, 1),
@@ -79,12 +114,19 @@ buyAset(P, Tile, h):-
     NTA is TileAsset + 1.
     tileAssetUpdater(Tile, NTA).
 
+sellAset(P, Tile, m):- 
+    canRedeemBasicCheck(P, Tile, 1),
+    tileAsset(Tile, TileAsset),
+    TileAsset =:= 0,
+    NTA is TileAsset - 1,
+    tileAssetUpdater(Tile, NTA).
+
 sellAset(P, Tile, r):-
     canBuyBasicCheck(P, Tile, 1),
     equalityCheck(Tile, 1),
     tileAsset(Tile, TileAsset),
-    TileAsset < 5, TileAsset >= 0,
-    TileAsset > 0 -> (NTA is TileAsset - 1; NTA is TileAsset),
+    TileAsset < 5, TileAsset > 0,
+    NTA is TileAsset - 1,
     tileAssetUpdater(Tile, NTA).
 
 sellAset(P, Tile, h):-
@@ -94,4 +136,5 @@ sellAset(P, Tile, h):-
     TileAsset =:= 4,
     NTA is TileAsset - 1.
     tileAssetUpdater(Tile, NTA).
+
     
