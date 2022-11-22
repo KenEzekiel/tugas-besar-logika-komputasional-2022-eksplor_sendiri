86950ca04976d57b6d4114b206d118bc83407176
Fatih20 <fnri39@protonmail.com>
2022-11-22 16:07:52 +0700

    Implement endTurn.

--- src/turns.pl
+++ src/turns.pl
@@ -1,16 +1,16 @@
 :- dynamic(turn/2).
 
-turn(p1, 1).
-turn(p2, 0).
+turn(v, 1).
+turn(w, 0).
 
 turnUpdater(P, NT):-
     retractall(turn(P, _)),
     asserta(turn(P, NT)).
 
-changeTurn :-
-    turn(p1, T1),
-    turn(p2, T2),
-    (T1 =:= 1 -> turnUpdater(p1, 0) ; turnUpdater(p1, 1)),
-    (T2 =:= 1 -> turnUpdater(p2, 0) ; turnUpdater(p2, 1)).
+endTurn :-
+    turn(v, T1),
+    turn(w, T2),
+    (T1 =:= 1 -> turnUpdater(v, 0) ; turnUpdater(w, 1)),
+    (T2 =:= 1 -> turnUpdater(w, 0) ; turnUpdater(v, 1)).
 
 
