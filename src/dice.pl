ce69ca1bdf82f3196372b2d0b805f8e0a983f38e
Fatih20 <fnri39@protonmail.com>
2022-11-22 15:35:44 +0700

    Debugged dice.

--- src/dice.pl
+++ src/dice.pl
@@ -15,40 +15,40 @@ resetDoubleAmount(P) :-
 rollDice(Res1, Res2, Double):-
     random(1, 6, Res1),
     random(1, 6, Res2),
-    write("Dadu 1: "),
-    write(Res1), write("."), nl,
-    write("Dadu 2: "),
-    write(Res2), write("."), nl,
+    write('Dadu 1: '),
+    write(Res1), write('.'), nl,
+    write('Dadu 2: '),
+    write(Res2), write('.'), nl,
     (Res1 \== Res2 -> Double is 0 ; Double is 1).
 
 doubleAmount(v, 0).
 doubleAmount(w, 0).
 
 throwDice :-
-    throwDiceW(Double).
+    throwDiceW(Double), !.
 
 % Karena setelah lempar dadu pemain masih bisa jual aset, masih harus bayar sewa, dll, logika untuk pergantian turn bukan di sini, tapi di throwDice. 
 % Logika untuk masuk penjara setelah 3 kali double ada di sini
 % Ini wrapper.
 throwDiceW(Double) :- 
     turn(PMoving, 1),
-    write("Sekarang adalah giliran pemain "),
-    write(PMoving), write("."), nl.
-    (isPJailed(PMoving, 1) -> throwDiceFree(P) ; throwDiceJail(P)).
+    write('Sekarang adalah giliran pemain '),
+    write(PMoving), write('.'), nl,
+    (isPJailed(PMoving, 1) -> throwDiceJail(P) ; throwDiceFree(P)), !.
 
 throwDiceJail(P) :-
     incrementTurnInJail(P),
     rollDice(Res1, Res2, Double),
     turnInJail(P, TJ),
-    (Double =:= 1 -> getUnjailed(P), write("Selamat, anda telah bebas dari penjara") ; (TJ =:= 3 -> write("Telah ada di penjara dalam 3 turn, anda otomatis bebas") ; write("Gagal mendapat double, masih dipenjara"))).
+    (Double =:= 1 -> getUnjailed(P), write('Selamat, anda telah bebas dari penjara') ; (TJ =:= 3 -> write('Telah ada di penjara dalam 3 turn, anda otomatis bebas') ; write('Gagal mendapat double, masih dipenjara'))).
 
 
 throwDiceFree(P) :-
     rollDice(Res1, Res2, Double),
     Res is Res1 + Res2,
-    (Double =:= 1 -> write("Double!"), incrementDoubleAmount(P) ; true),
-    (doubleAmount(P, 3) -> (getJailed(P), resetDoubleAmount(P)), write("Anda masuk penjara karena mendapat Double 3 kali berturut-turut") ; (
-    write("Anda maju sebanyak "), write(Res), write("langkah."), nl, movePlayerStep(P, Res))).
+    (Double =:= 1 -> write('Double! '), incrementDoubleAmount(P) ; true),
+    (doubleAmount(P, 3) -> (getJailed(P), resetDoubleAmount(P)), write('Anda masuk penjara karena mendapat Double 3 kali berturut-turut') ; (
+    write('Anda maju sebanyak '), write(Res), write(' langkah.'), nl, movePlayerStep(P, Res))), !.
     
 
 
