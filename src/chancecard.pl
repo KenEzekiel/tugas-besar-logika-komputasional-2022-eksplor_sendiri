e274d0defd1b1bd7c2b2b8c9f335fffb798d86a5
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2022-11-22 21:41:23 +0700

    Implemented mundurTigaLangkah and updated kartupajak

--- src/chancecard.pl
+++ src/chancecard.pl
@@ -4,10 +4,11 @@ chancecard(tax, Player) :- kartupajak(Player).
 chancecard(prize, Player) :- kartuhadiah(Player).
 chancecard(getout, Player) :- getkeluarpenjara(Player, CardInventory).
 chancecard(gotojail, Player) :- pergikepenjara(Player).
-chancecard(angel, Player) :-
+chancecard(backthreestep, Player) :- mundurTigaLangkah(Player).
+/*chancecard(angel, Player) :- */
 
 kartupajak(P) :-
-    movePlayerTo(P, tx),
+    moveToNearestTax(P),
     payTax(P).
 
 kartuhadiah(P) :-
@@ -15,17 +16,19 @@ kartuhadiah(P) :-
     plusMoney(P, M).
 
 getkeluarpenjara(P, CardInventory) :-
-    insertElmtLast(CardInventory, kartukeluarpenjara, A),
+    insertElmtLast(CardInventory, getout, A),
     CardInventory = A.
 
 usekeluarpenjara(P, CardInventory) :-
-    deleteElmt(CardInventory, kartukeluarpenjara, A),
     isPJailed(P, X),
     X =:= 1,
+    deleteElmt(CardInventory, getout, A),
     getUnjailed(P),
     CardInventory = A.
-    
 
 pergikepenjara(P) :-
     getJailed(P),
     endTurn(P).
+
+mundurTigaLangkah(P) :-
+    movePlayerStep(P, -3).
