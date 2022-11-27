10977369a152a6fe0cf5703fe3592a8254d66ea6
Fakhrimm <16521155@mahasiswa.itb.ac.id>
2022-11-28 00:13:08 +0700

    Update command,pl

--- src/command.pl
+++ src/command.pl
@@ -13,6 +13,7 @@
 
 dynamic(playerState/2).
 
+
 playerState(v, diceThrown).
 playerState(w, diceThrown).
 playerState(w, jailed).
@@ -22,29 +23,85 @@ stateChanger(Player, NewState):-
     retractall(turn(Player, _)),
     asserta(turn(Player, NewState)).
 
-
-
 cash:-
     turn(Player, 1),
     balance(Player, Balance),
     format('Uang: ~d', [Balance]), !.
 
-buy:-
+getParam(Param, X):-
+    Param == tanah, 
+    X is 0, !.
+
+getParam(Param, X):-
+    Param == bangunan1, 
+    X is 1, !.
+
+getParam(Param, X):-
+    Param == bangunan2, 
+    X is 2, !.
+
+getParam(Param, X):-
+    Param == bangunan3, 
+    X is 3, !.
+
+getParam(Param, X):-
+    Param == landmark, 
+    X is 4, !.
+
+getParam(_, _):-
+    fail.
+
+buy(Param):-
     turn(Player, 1),
     playerState(Player, diceThrown),
     location(Player, Tile),
-    tileAsset(Tile, Status, Owner) -> (
-        checkPropertyDetail(Tile)
+    getParam(Param, X),
+    tileAsset(Tile, State, Owner),
+    balance(Player, Balance),
+    Owner \== Player -> (
+        Owner \== none -> (
+            write('Gunakan command acquisition')
+        ) ; (
+            X =:= 0 -> (
+                buyTile(Player, Tile) -> (
+                    format('Anda berhasil membeli tanah ~d', [Tile])
+                ) ; (
+                    write('Uang anda tidak cukup')
+                )
+            ) ; (
+                X =:= 4 -> (
+                    write('Anda harus mempunyai 3 bangunan terlebih dahulu')
+                ) ; (
+                    propertyPrices(Tile, Prices), 
+                    sumUntil(Prices, X, Sum),
+                    Sum > Balance -> (
+                        write('Uang anda tidak mencukupi')
+                    ) ; (
+                        buyTile(Player, Tile),
+                        repeat,
+                        buyAset(Player, Tile, r),
+                        tileAsset(Tile, X, Player)
+                    ) 
+                )
+            )
+        )
     ) ; (
-        write('Deadmanlands')
+        X =:= 0 -> (
+            write('Anda sudah memiliki tanah')
+        ) ; (
+            X =:= 4 -> (
+                write('Anda harus mempunyai 3 bangunan terlebih dahulu')
+            ) ; (
+                propertyPrices(Tile, Prices), 
+                sumUntil(Prices, X, Sum),
+                Sum > Balance -> (
+                    write('Uang anda tidak mencukupi')
+                ) ; (
+                    buyTile(Player, Tile),
+                    repeat,
+                    buyAset(Player, Tile, r),
+                    tileAsset(Tile, X, Player)
+                ) 
+            )
+        )
     ).
-
-
-
-
-
-/*
-buy:-
-    turn(Player, 1),
-    buy.
-*/
