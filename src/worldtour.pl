6a33da064afd42e341c6ecbcc27a4f74143eca29
Fakhrimm <16521155@mahasiswa.itb.ac.id>
2022-11-28 14:55:49 +0700

    Update worldtour.pl

---
+++ src/worldtour.pl
@@ -0,0 +1,7 @@
+worldTour:-
+    write('Kamu akan dibawa keliling dunia di punggung Stormterror, dibantu oleh Venti ?'), nl,
+    write('Tetapi kamu harus membelikan Venti sejumlah minuman seharga 5000 mora.'), nl,
+    write('1. Siapa takut'),
+    write('2. Mora ku sedikit :<'),
+    read(Input),
+    Input = 
